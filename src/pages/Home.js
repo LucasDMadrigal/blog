07ca7ee2daf3d009c5f431d4b454a180f5fad45a
lucasdmadrigal <lucasmadrigalb@gmail.com>
2021-07-08 21:40:14 -0300

    escalando 1440

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,18 +16,20 @@ export class Home extends Component {
                 </header>
                 <section className="container about" >
                     <h1 id="about">Sobre mi</h1>
-                    <p>Soy un Desarrollador web vivo en Tucumán, Argentina.<br />
+                    <p>Soy un Desarrollador web vivo en Tucumán, Argentina. </p>
+
+                    <p>
                         Mi formacion fue en parte en la universidad Tecnologica Nacional donde curse la carrera de Tecnico Universitario en programacion; Con grandes docentes y profesionales adquiri los conocimientos basicos para empezar a desarrollarme como profesional.
                     </p>
-                    <br />
+
                     <p>
                         Otra de mis grandes escuelas fue Platzi. Los profesores y la comunidad de Platzi me dieron un panorama mucho mas claro de la industria y como estamos y a donde vamos a nivel latinoamerica, y aprendi a manejar lenguajes de la web (HTML, CSS, JavaScript) y conocimientos que en la universidad no tuve la oportunidad de profundizar (Algoritmos, diseño Web, responsive design).
                     </p>
-                    <br />
+
                     <p>
                         Tengo un gran sentido de la curiosidad y disfruto mucho de aprender cosas nuevas. Eso me lleva a leer e investigar con gran pasion. Cuando tengo una duda no me detengo hasta resolverla.
                     </p>
-                    <br />
+
                     <p>
                         Me concidero una persona espiritual, y eso sumado a mi curiosidad me llevaron a estudiar la Biblia, donde encontre resupuestas a muchas cosas. Hace unos años que asisto y soy parte de la Iglesia Adventista del Septimo Dia.
                     </p>
