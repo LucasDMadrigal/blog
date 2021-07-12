cb8215cd000aeeab431ff6aecf7763fae8f8755f
lucasdmadrigal <lucasmadrigalb@gmail.com>
2021-07-12 13:29:54 -0300

    transicion responsive completo

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ function Contact() {
     function sendEmail(e) {
         e.preventDefault();
 
-        emailjs.sendForm('service_pkg38il', 'template_p70wyzr', e.target, 'user_qIJzZjoZMlupoiS5f4Ta4')
+        emailjs.sendForm('service_aic3iy5', 'template_p70wyzr', e.target, 'user_qIJzZjoZMlupoiS5f4Ta4')
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
@@ -21,17 +21,17 @@ function Contact() {
         <main className="container-fluid">
             <h3>Contacto</h3>
             <form className="contact-form" onSubmit={sendEmail}>
-                <label className="form-label">Name</label>
-                {/* <span className="required_message">Este campo no puede estar vacio</span> */}
-                <input className="form-control" type="text" name="user_name" required />
-                <label>Email</label>
-                {/* <span className="required_message">Este campo no puede estar vacio</span> */}
-                <input className="form-control" type="email" placeholder="name@example.com" name="user_email" required />
-                <label>Message</label>
-                {/* <span className="required_message">Este campo no puede estar vacio</span> */}
-                <textarea className="form-control textarea" placeholder="Escribeme y estaremos en contacto..." name="message" required />
-                {/* <span className="required_message">verifica y completa el formulario, por favor</span> */}
-                <input className="btn btn-form" type="submit" value="Send" />
+                <div className="input_container">
+                    <label className="form-label">Name</label>
+                    <input className="form-control" type="text" name="user_name" required />
+                    <label>Email</label>
+                    <input className="form-control" type="email" placeholder="name@example.com" name="user_email" required />
+                </div>
+                <div className="message_container">
+                    <label>Message</label>
+                    <textarea className="form-control textarea" placeholder="Escribeme y estaremos en contacto..." name="message" required />
+                    <input className="btn btn-form" type="submit" value="Send" />
+                </div>
             </form>
         </main>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react'
-import ProfilePhoto from '../assets/images/profile-photo.jpg'
-import Avatar from '../assets/images/myAvatar.svg'
-import './styles/Home.css'
+import React, { Component } from 'react';
+import Avatar from '../assets/images/myAvatar.svg';
+import Avatar2 from '../assets/images/myAvatar-1.svg';
+import './styles/Home.css';
+
 export class Home extends Component {
     render() {
         return (
             <main className="container-fluid home">
                 <header id="" className="hero">
                     <picture>
-                        <img className="hero_photo" src={Avatar} alt="hero_me" />
+                        {/* <img className="hero_photo" src={Avatar} alt="hero_me" /> */}
+                        <img className="hero_photo" src={Avatar2} alt="hero_me" />
                     </picture>
                     <h3 className="title">Mi nombre es </h3>
                     <h1 className="title">Lucas Madrigal</h1>
                     <h5 className="sub-title">y soy frontend developer</h5>
                 </header>
                 <section className="container about" >
-                    <h1 id="about">Sobre mi</h1>
+                    <h2 id="about">Sobre mi</h2>
                     <p>Soy un Desarrollador web vivo en Tucumán, Argentina. </p>
 
                     <p>
